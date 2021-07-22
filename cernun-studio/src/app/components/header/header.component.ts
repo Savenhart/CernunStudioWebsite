3d894a6b5cee40e5b69d58f49a952de21760166a
Theo Lounissi <Theo.lounissi@gmail.com>
2021-07-22 21:24:42 +0200

    Login/Logout

--- cernun-studio/src/app/components/header/header.component.ts
+++ cernun-studio/src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { AuthenficationService } from 'src/app/services/authenfication.service';
 
 @Component({
   selector: 'app-header',
@@ -7,15 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  currentUser!: string;
+  currentUser!: User;
 
-  constructor() { }
+  constructor(private authenficationService: AuthenficationService, private router: Router) {
+    this.authenficationService.currentUser.subscribe(x => this.currentUser = x)
+   }
 
   ngOnInit(): void {
   }
 
   logout(){
-    
+    this.authenficationService.logout();
+    this.router.navigate(['/home']);
   }
 
 }
--- cernun-studio/src/app/components/login/login.component.ts
+++ cernun-studio/src/app/components/login/login.component.ts
@@ -1,22 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { User } from 'src/app/models/user';
+import { AuthenficationService } from 'src/app/services/authenfication.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
   loginForm: FormGroup = this.formBuilder.group({});
+  user: User = new User();
 
-  constructor(private formBuilder: FormBuilder) { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder, private authenficationService: AuthenficationService) {
   }
 
-  onSubmit(){
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      userName: '',
+      password: '',
+    });
+  }
+
+  get f() { return this.loginForm.controls; }
+
+  onSubmit() {
+    this.user.userName = this.f.userName.value;
+    this.user.password = this.f.password.value;
     
+    this.authenficationService.login(this.user)
   }
 
 }
