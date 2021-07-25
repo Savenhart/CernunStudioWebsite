308d2f89f84dcb98b6a7cfc84edabfec47d77224
Theo Lounissi <Theo.lounissi@gmail.com>
2021-07-25 20:18:29 +0200

    Started api rest
    Get and Post request on users table (json file for now)

--- Api/server.ts
+++ Api/server.ts
@@ -3,24 +3,48 @@ const path = require("path");
 const app = express(),
   bodyparser = require("body-parser");
 const port = 3080;
+const fs = require("fs");
 
 const users: string[] = [];
 
 app.use(bodyparser.json());
 app.use(express.static(path.join(__dirname, "../cernunstudio/build")));
 
-app.get("/Api/users", (req: any, res: { json: (arg0: string[]) => void }) => {
-  console.log("api/users called!");
-  res.json(users);
+app.get("/Api/users", (req: any, res: { json: (arg0: any) => void }) => {
+  fs.readFile("./db.json", (err: any, json: string) => {
+    let obj = JSON.parse(json);
+    res.json(obj);
+  });
 });
 
 app.post(
-  "/Api/user",
+  "/Api/users",
   (req: { body: { user: any } }, res: { json: (arg0: string) => void }) => {
     const user = req.body.user;
     console.log("Adding user:::::", user);
-    users.push(user);
-    res.json("user added");
+    let data = user;
+    fs.readFile("./db.json", (err: any, json: string) => {
+      if (err) {
+        console.log(err);
+      } else {
+        let obj = {
+          users: [] as any
+        }
+      obj = JSON.parse(json);
+      obj.users.push(data)
+
+      data = JSON.stringify(obj);
+
+        fs.writeFile("./db.json", data, (err: any) => {
+          if (err) console.log(err);
+          else {
+            console.log("File written successfully\n");
+            users.push(user);
+            res.json("user added");
+          }
+        });
+      }
+    });
   }
 );
 
