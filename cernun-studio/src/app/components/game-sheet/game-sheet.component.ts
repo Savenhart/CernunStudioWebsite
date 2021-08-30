bcb1a68e0df84d715288d4ae3fba6fb9c0522a0a
Theo Lounissi <Theo.lounissi@gmail.com>
2021-08-30 23:33:21 +0200

    Show game img on front

--- cernun-studio/src/app/components/game-sheet/game-sheet.component.ts
+++ cernun-studio/src/app/components/game-sheet/game-sheet.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
+import { GameImageModel } from 'src/app/models/game-image-model';
 import { GameSheet } from 'src/app/models/game-sheet';
 import { GameSheetService } from 'src/app/services/game-sheet.service';
 
@@ -10,16 +11,32 @@ import { GameSheetService } from 'src/app/services/game-sheet.service';
   styleUrls: ['./game-sheet.component.css'],
 })
 export class GameSheetComponent implements OnInit {
-
   id!: number;
 
-  game: GameSheet = new GameSheet;
+  game: GameSheet = new GameSheet();
 
-  constructor(private gameSheetService: GameSheetService, private route: ActivatedRoute) {
+  constructor(
+    private gameSheetService: GameSheetService,
+    private route: ActivatedRoute
+  ) {
     this.id = this.route.snapshot.paramMap.get('id') as unknown as number;
-    this.gameSheetService.getById(this.id).subscribe(data => {this.game = new GameSheet(data)});   
-    }
-
-  ngOnInit(): void {
+    this.gameSheetService.getById(this.id).subscribe((data: any) => {
+      let gameImages: GameImageModel[] = [];
+      
+      for (const img of data.image) {
+        gameImages.push(img);
+      }
+      this.game = new GameSheet({
+        id: data.id,
+        name: data.name,
+        content: data.content,
+        date: data.date,
+        images: gameImages,
+        link: data.link
+      });
+    });
+    
   }
+
+  ngOnInit(): void {}
 }
